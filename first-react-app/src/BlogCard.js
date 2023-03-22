3e82945b52f35845dfca3575e410e157421f725d
Nithin Nitz <nithinraj10@hotmail.com>
2023-03-22 16:16:07 +0000

    CSS modules

--- first-react-app/src/BlogCard.js
+++ first-react-app/src/BlogCard.js
@@ -2,11 +2,14 @@ import React from 'react';
 
 import { dumpLogs } from './Utils';
 
+// import './BlogCard.css';
+import classes from './BlogCard.module.css';
+
 //Functional component
 const BlogCard = (props) =>{
     dumpLogs(props);
     return(
-        <div className="BlogCard">
+        <div className={classes.NewBlogCard}>
         <h3>{props.title}</h3>
         <p>{props.description}</p>
       </div> 
