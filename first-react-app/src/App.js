429c76eb7292452a0a4378237055af41a933416b
Nithin Nitz <nithinraj10@hotmail.com>
2023-03-29 16:07:39 +0000

    Class based component

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -3,30 +3,35 @@ import './App.css';
 import './App.css';
 import BlogCard from './BlogCard';
 import { isArrayEmpty } from './Utils';
+import React , { Component } from 'react';
 
-function App() {
-  const firstName = 'Johnny';
-  const lastName = 'Depp';
-  const age = 59;
-  const job = 'Actor/Pirate';
-  const hobby = 'Reading books';
-
-  const mArr = [1,2,3,4]
-  const mObj = {
-    name: 'Robert Downey Jr',
-    age: 57
+class App extends Component {
+  state = {
+    showBlogs : true
   }
 
-  const inputPlaceholder = 'Enter your Details'
+  // const firstName = 'Johnny';
+  // const lastName = 'Depp';
+  // const age = 59;
+  // const job = 'Actor/Pirate';
+  // const hobby = 'Reading books';
 
-  const getHobby = (hobby) => `${hobby}`
+  // const mArr = [1,2,3,4]
+  // const mObj = {
+  //   name: 'Robert Downey Jr',
+  //   age: 57
+  // }
 
-  const detailsInputBox = <input placeholder = {inputPlaceholder} 
-  autoComplete />
+  // const inputPlaceholder = 'Enter your Details'
+
+  // const getHobby = (hobby) => `${hobby}`
+
+  // const detailsInputBox = <input placeholder = {inputPlaceholder} 
+  // autoComplete />
 
   // <-------------------------------------->
 
-  const blogArr = [
+  blogArr = [
     {
       id: 1,
       title: 'Blog Title 1',
@@ -44,7 +49,9 @@ function App() {
     }
   ]
 
-  const blogCards = isArrayEmpty(blogArr) ? [] : blogArr.map((item, pos) => {
+ 
+
+  blogCards = isArrayEmpty(this.blogArr) ? [] : this.blogArr.map((item, pos) => {
     // console.log(item);
 
     return(
@@ -56,33 +63,51 @@ function App() {
     )
   })
 
+  onHideBtnClick = () => {
+    this.setState({showBlogs: false});
+    console.log(this.showBlogs);
+  }
 
-  return (
-    <div className="App">
-      <h3>Full Name: {`${firstName} ${lastName}`}</h3>
-      <p>Age: {age}</p>
-      <p>Job: {job}</p>
-      <p>Hobby: {getHobby(hobby)}</p>
-
-      {mArr[0]}
-      {/* {detailsInputBox} */}
+  render() {
+    return(
+      <div className="App">
+        <button onClick={this.onHideBtnClick}>Hide List</button>
+      <br></br>
       {
-        mArr[0] > 0 ? "True" : "False"
+        this.showBlogs ? this.blogCards : null
       }
+      </div>
+    );
+  }
 
-      <h3>Full Name: {mObj.name} </h3>
-      <p>Age: {mObj.age} </p>
+  // return (
+  //   <div className="App">
+  //     // <h3>Full Name: {`${firstName} ${lastName}`}</h3>
+  //     // <p>Age: {age}</p>
+  //     // <p>Job: {job}</p>
+  //     // <p>Hobby: {getHobby(hobby)}</p>
+
+  //     // {mArr[0]}
+  //     // {/* {detailsInputBox} */}
+  //     // {
+  //     //   mArr[0] > 0 ? "True" : "False"
+  //     // }
+
+  //     // <h3>Full Name: {mObj.name} </h3>
+  //     // <p>Age: {mObj.age} </p>
     
     
-    {/* <-------------------------------------------> */}
-      {/* <div className="BlogCard">
-        <h3>{blogObj.title}</h3>
-        <p>{blogObj.description}</p>
-      </div> */}
-      {blogCards}
+  //   {/* <-------------------------------------------> */}
+  //     {/* <div className="BlogCard">
+  //       <h3>{blogObj.title}</h3>
+  //       <p>{blogObj.description}</p>
+  //     </div> */}
+  //     {blogCards}
+  //     <br></br>
+  //     <button onClick={onHideBtnClick}>Hide List</button>
     
-    </div>
-  );
+  //   </div>
+  // );
 }
 
 export default App;
