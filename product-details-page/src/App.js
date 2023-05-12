f1da9e4cae082b617dfad78bdf2f03ce11232324
Nithin Raj <nithinraj10@hotmail.com>
2023-05-12 21:44:23 +0530

    💄: Updating the Navbar

---
+++ product-details-page/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import classes from './App.module.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <nav className={classes.Topbar}>
+          <img src='https://images.pexels.com/photos/2235130/pexels-photo-2235130.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='logo'></img>
+        </nav>
+      </header>
+    </div>
+  );
+}
+
+export default App;
