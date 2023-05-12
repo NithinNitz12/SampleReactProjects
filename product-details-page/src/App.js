9c7b2e46b5554d95aee6f98c775f6dd592da8997
Nithin Raj <nithinraj10@hotmail.com>
2023-05-12 22:02:08 +0530

    💄: Adding Fonts

--- product-details-page/src/App.js
+++ product-details-page/src/App.js
@@ -1,14 +1,27 @@
 import React from 'react'
 import classes from './App.module.css';
+import ProductData from './ProductData';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <nav className={classes.Topbar}>
-          <img src='https://images.pexels.com/photos/2235130/pexels-photo-2235130.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='logo'></img>
+          <img src='https://images.pexels.com/photos/2235130/pexels-photo-2235130.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='logo'/>
         </nav>
       </header>
+
+      <div className={classes.MainContainer}>
+        <div className={classes.ProductPreview}>
+          <img src="https://imgur.com/iOeUBV7.png" alt='Product Preview'/>
+        </div>
+
+        <div className={classes.ProductData}>
+          <h1 className={classes.ProductTitle}>{ProductData.title}</h1>
+        </div>
+
+      </div>
+
     </div>
   );
 }
