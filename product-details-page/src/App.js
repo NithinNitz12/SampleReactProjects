4ad247f7579d70cf2edd97c0cbafd4f34e8a439b
Nithin Raj <nithinraj10@hotmail.com>
2023-05-18 21:37:44 +0530

    💄Added color straps, watch features, buy now

--- product-details-page/src/App.js
+++ product-details-page/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import classes from './App.module.css';
 import ProductData from './ProductData';
+import logo from './logo.svg'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <nav className={classes.Topbar}>
-          <img src='https://images.pexels.com/photos/2235130/pexels-photo-2235130.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='logo'/>
+          {/* <img src='https://images.pexels.com/photos/2235130/pexels-photo-2235130.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='logo'/> */}
+          <img src={logo} alt='logo'/>
         </nav>
       </header>
 
@@ -18,6 +20,21 @@ function App() {
 
         <div className={classes.ProductData}>
           <h1 className={classes.ProductTitle}>{ProductData.title}</h1>
+          <p className={classes.ProductDescription}>{ProductData.description}</p>
+
+          <h3 className={classes.SectionHeading}>Select Color</h3>
+          <div>
+            <img className={[classes.ProductImage, classes.SelectedProductImage].join(' ')} src='https://imgur.com/iOeUBV7.png' alt='Black Colored Watch'/>
+            <img className={classes.ProductImage} src='https://imgur.com/PTgQlim.png' alt='Red Colored Watch'/>
+            <img className={classes.ProductImage} src='https://imgur.com/Mplj1YR.png' alt='Blue Colored Watch'/>
+            <img className={classes.ProductImage} src='https://imgur.com/xSIK4M8.png' alt='Purple Colored Watch'/>
+          </div>
+          <h3 className={classes.SectionHeading}>Features</h3>
+          <div>
+            <button className={[classes.FeatureItem, classes.SelectedFeatureItem].join(' ')}>Time</button>
+            <button className={classes.FeatureItem}>Heart Rate</button>
+          </div>
+          <button className={classes.PrimaryButton}>Buy Now</button>
         </div>
 
       </div>
