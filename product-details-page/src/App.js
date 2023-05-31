8a82ab4c66060e471a6b0ccbb71591844e49ed79
Nithin Raj <nithinraj10@hotmail.com>
2023-05-31 15:18:39 +0530

    ✨ Watch features completed

--- product-details-page/src/App.js
+++ product-details-page/src/App.js
@@ -4,6 +4,8 @@ import ProductData from './ProductData';
 import logo from './logo.svg'
 
 function App() {
+  const currentHour = new Date().getHours() > 9 ? new Date().getHours() : '0' + new Date().getHours();
+  const currentMinute = new Date().getMinutes > 9 ? new Date().getMinutes() : '0' + new Date().getMinutes();
   return (
     <div className="App">
       <header className="App-header">
@@ -16,6 +18,15 @@ function App() {
       <div className={classes.MainContainer}>
         <div className={classes.ProductPreview}>
           <img src="https://imgur.com/iOeUBV7.png" alt='Product Preview'/>
+
+          {/* <div className={classes.TimeSection}>
+            <p>{`${currentHour}:${currentMinute}`}</p>
+          </div> */}
+
+          <div className={classes.HeartBeatSection}>
+            <i class="fas fa-heartbeat"></i>
+            <p>78</p>
+          </div>
         </div>
 
         <div className={classes.ProductData}>
